df87dc95e00ff3ab10e823df70291ffe368ea94d
Thameem Ansari <thameem@apigee.com>
2014-05-03 19:53:03 -0700

    Adding static html page layouts

--- src/main/java/com/brainfotech/school2go/controller/HelloController.java
+++ src/main/java/com/brainfotech/school2go/controller/HelloController.java
@@ -1,5 +1,8 @@
 package com.brainfotech.school2go.controller;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,13 +14,16 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class HelloController {
 
-    @RequestMapping(value = { "/", "/welcome**" }, method = RequestMethod.GET)
+    protected static final Logger logger = LoggerFactory.getLogger(HelloController.class);
+
+    @RequestMapping(value = { "/", "/index**" }, method = RequestMethod.GET)
     public ModelAndView welcomePage() {
 
         ModelAndView model = new ModelAndView();
         model.addObject("title", "Spring Security Hello World");
         model.addObject("message", "This is welcome page!");
-        model.setViewName("hello");
+        logger.debug("Inside hello controller view.. sending to view 'login'");
+        model.setViewName("login");
         return model;
 
     }
